2420e5fa32c1197cdeeaf6cdf64aa86c4d9b0f1a
kellydunn <defaultstring@gmail.com>
2012-04-29 12:48:37 -0700

    [src] Adding in tb303 for kicks

--- src/ginger/core.clj
+++ src/ginger/core.clj
@@ -9,13 +9,28 @@
 
 (definst foo [freq 440] (saw freq))
 
+(defsynth tb303 [note 60 wave 1
+                 cutoff 100 r 0.9
+                 attack 0.101 decay 0 sustain 1 release 0.4
+                 env 200 gate 0 vol 0.8]
+  (let [freq (midicps note)
+        freqs [freq (* 1.01 freq)]
+        vol-env (env-gen (adsr attack decay sustain release)
+                         (line:kr 1 0 (+ attack decay release))
+                         :action FREE)
+        fil-env (env-gen (perc))
+        fil-cutoff (+ cutoff (* env fil-env))
+        waves [(* vol-env (saw freqs))
+               (* vol-env [(pulse (first freqs) 0.5) (lf-tri (second freqs))])]]
+        (out 0 (* [vol vol] (rlpf (select wave (apply + waves)) fil-cutoff r)))))
+
 (defn play-note [m x y]
   (mled/led-on m x y)
-  (foo (+ (* x 50) (* y 150))))
+  (tb303 :note (+ (* x 20) 10)))
 
 (defn kill-note [m x y]
   (mled/led-off m x y)
-  (kill foo))
+  (kill tb303))
 
 (mevent/on-press m (fn [x y] (play-note m x y)) "*")
 (mevent/on-release m (fn [x y] (kill-note m x y)) "*")
