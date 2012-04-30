082c58a3b76ebaed3edba2bcf70f3c617040e41a
kellydunn <defaultstring@gmail.com>
2012-04-30 01:44:44 -0700

    [src] Autoloading instruments :)

--- src/ginger/instruments.clj
+++ src/ginger/instruments.clj
@@ -1,7 +1,19 @@
 (ns ginger.instruments
   (:use [overtone.live]))
 
-(import [ginger.instruments.foobar])
+;; TODO make load path configurable
+(def directory (clojure.java.io/file "/home/kelly/.ginger/instruments"))
+(def files (file-seq directory))
+(def instrument-files
+  (filter (fn [^java.io.File file]
+            (.isFile file)) files))
+
+(def load-instrument-files
+  (apply (fn [^java.io.File file]
+           (load-file (.getAbsolutePath file))) instrument-files))
+
+;; TODO Fix so that instruments don't auto play after loading them :P
+(load-instrument-files)
 
 (ginger.instruments.foobar/foobar)
 (kill ginger.instruments.foobar/foobar)
