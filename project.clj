e427909dcbab5799454149eb5d012aebc07bb5d3
kellydunn <defaultstring@gmail.com>
2012-04-29 21:04:44 -0700

    [project] Adding in the .ginger classpath

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
                  [polynome "0.3-SNAPSHOT"]
                  [overtone "0.6.0"]]
   :dev-dependencies [[swank-clojure "1.4.0"]]
-  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
-  :extra-classpath-dirs ["/usr/lib/jvm/java-6-sun/lib/tools.jar"])
+  :extra-classpath-dirs ["~/.ginger"])
