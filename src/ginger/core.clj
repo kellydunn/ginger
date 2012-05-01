d57bd3b735009b450ce7ec55d5fa54721bba85d3
kellydunn <defaultstring@gmail.com>
2012-04-30 23:52:06 -0700

    [src] Removing older examples, including simple beep example provided by overtone examples

--- src/ginger/core.clj
+++ src/ginger/core.clj
@@ -9,31 +9,19 @@
 (defonce instruments ginger.instruments/load-instrument-files)
 (defonce m (mcore/connect "/dev/ttyUSB0"))
 
+(dissoc instruments)
+
 ;; TODO load first instrument to monome
 ;; TODO allow users to switch between instruments
 
-(defsynth tb303 [note 60 wave 1
-                 cutoff 100 r 0.9
-                 attack 0.101 decay 0 sustain 1 release 0.4
-                 env 200 gate 0 vol 0.8]
-  (let [freq (midicps note)
-        freqs [freq (* 1.01 freq)]
-        vol-env (env-gen (adsr attack decay sustain release)
-                         (line:kr 1 0 (+ attack decay release))
-                         :action FREE)
-        fil-env (env-gen (perc))
-        fil-cutoff (+ cutoff (* env fil-env))
-        waves [(* vol-env (saw freqs))
-               (* vol-env [(pulse (first freqs) 0.5) (lf-tri (second freqs))])]]
-        (out 0 (* [vol vol] (rlpf (select wave (apply + waves)) fil-cutoff r)))))
-
+;; TODO allow notes to play scales defined by the instrument
 (defn play-note [m x y]
   (mled/led-on m x y)
-  (tb303 :note (+ (+ (* x 1) (* y 8)) 20)))
+  (beep :note (+ (+ (* x 1) (* y 8)) 20)))
 
 (defn kill-note [m x y]
   (mled/led-off m x y)
-  (kill tb303))
+  (beep overpad))
 
 (mevent/on-press m (fn [x y] (play-note m x y)) "*")
 (mevent/on-release m (fn [x y] (kill-note m x y)) "*")
