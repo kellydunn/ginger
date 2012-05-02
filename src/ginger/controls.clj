d768a8abb1172ad867de9df6cfea513eba09e44f
kellydunn <defaultstring@gmail.com>
2012-05-01 20:11:40 -0700

    [src] Adding in the ability to load in first instrument.

---
+++ src/ginger/controls.clj
@@ -0,0 +1,1 @@
+(ns ginger.controls)
--- src/ginger/core.clj
+++ src/ginger/core.clj
@@ -4,24 +4,27 @@
             [monome-serial.led :as mled]
             [monome-serial.event-handlers :as mevent]
             [overtone.core :as overtone]
-            [ginger.instruments]))
+            [ginger.instruments])
+  (gen-class))
 
 (defonce instruments ginger.instruments/load-instrument-files)
 (defonce m (mcore/connect "/dev/ttyUSB0"))
 
+(intern 'ginger.core 'instrument-index 1)
 (dissoc instruments)
 
-;; TODO load first instrument to monome
 ;; TODO allow users to switch between instruments
+(def current-instrument
+  (nth instruments instrument-index))
 
 ;; TODO allow notes to play scales defined by the instrument
 (defn play-note [m x y]
   (mled/led-on m x y)
-  (beep :note (+ (+ (* x 1) (* y 8)) 20)))
+  (current-instrument :note (+ (+ (* x 1) (* y 8)) 20)))
 
 (defn kill-note [m x y]
   (mled/led-off m x y)
-  (beep overpad))
+  (kill current-instrument))
 
 (mevent/on-press m (fn [x y] (play-note m x y)) "*")
 (mevent/on-release m (fn [x y] (kill-note m x y)) "*")
