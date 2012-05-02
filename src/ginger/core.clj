c66c8e7d271112a52ed56119e46b4c66ad5afc07
kellydunn <defaultstring@gmail.com>
2012-05-01 23:23:10 -0700

    [src] Instrument switching works now

--- src/ginger/core.clj
+++ src/ginger/core.clj
@@ -4,35 +4,32 @@
             [monome-serial.led :as mled]
             [monome-serial.event-handlers :as mevent]
             [overtone.core :as overtone]
-            [ginger.instruments])
+            [ginger.controls :as controls])
   (gen-class))
 
-(defonce instruments ginger.instruments/load-instrument-files)
 (defonce m (mcore/connect "/dev/ttyUSB0"))
 
-(intern 'ginger.core 'instrument-index 0)
-(dissoc instruments)
-
-;; TODO allow users to switch between instruments
-(def current-instrument
-  (nth instruments instrument-index))
+;; TODO Seems uneeded.
+;;      Learning clojure woo!
+(dissoc controls/instruments)
 
 (defn play-note [m x y]
   (mled/led-on m x y)
-  (current-instrument :note (+ (+ (* x 1) (* y 8)) 20)))
+  ((controls/get-instrument (deref controls/current-index)) :note (+ (+ (* x 1) (* y 8)) 25)))
 
 (defn kill-note [m x y]
   (mled/led-off m x y)
-  (kill current-instrument))
+  (kill (controls/get-instrument (deref controls/current-index))))
 
 (defn delegate-on-press [m x y]
   (when (> y 0)
-    (play-note m x y)))
+    (play-note m x y))
+  (when (= y 0)
+    (controls/switch-instrument-index m x y)))
 
 (defn delegate-on-release [m x y]
   (when (> y 0)
     (kill-note m x y)))
 
-;; TODO allow notes to play scales defined by the instrument
 (mevent/on-press m (fn [x y] (delegate-on-press m x y)) "*")
 (mevent/on-release m (fn [x y] (delegate-on-release m x y)) "*")
